7cd6287ad38b79299f768d2850c0a380464c7aa6
Rhys Canady <rhysdaniel064@gmail.com>
2016-12-08 09:13:45 -0600

    001

---
+++ src/canady_linkedlist/Canady_LinkedList.java
@@ -0,0 +1,56 @@
+package canady_linkedlist;
+
+import java.util.*;
+
+public class Canady_LinkedList {
+
+    static Element e5 = new Element(null, 0, "e5", "d", "z");
+    static Element e4 = new Element(e5, 1, "e4", "s", "y");
+    static Element e3 = new Element(e4, 2, "e3", "b", "h");
+    static Element e2 = new Element(e3, 3, "e2", "c", "n");
+    static Element e1 = new Element(e2, 4, "e1", "x", "m");
+    static Element first = e1;
+    static Element last = e5;
+
+    public static void main(String[] args) {
+        listt();
+    }
+
+    static void add(Element e) {
+        Scanner sc = new Scanner(System.in);
+        int input1;
+        String input2;
+        Element newitem;
+        newitem = new Element(null, 0, "", "", "");
+        System.out.println("What is the new element's name?");
+        input2 = sc.nextLine();
+        newitem.setName(input2);
+        System.out.println("What is the power level of the new element?");
+        input1 = sc.nextInt();
+        newitem.setPower(input1);
+        System.out.println("What is the species of the new element?");
+        input2 = sc.nextLine();
+        newitem.setSpec(input2);
+        System.out.println("What is the ability of the new element?");
+        input2 = sc.nextLine();
+        newitem.setAbility(input2);
+    }
+
+    static void del(int e) {
+
+    }
+
+    static void delEnd(int e) {
+
+    }
+
+    static void listt() {
+        Element placeholder;
+        placeholder = first;
+        while (placeholder.tail != null) {
+            System.out.println(placeholder.toString());
+            placeholder = placeholder.tail;
+        }
+        System.out.println(placeholder.toString());
+    }
+}
